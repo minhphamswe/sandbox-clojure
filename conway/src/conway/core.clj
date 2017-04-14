e4242c4b77d403aea38b38c310ed02373f797df8
Minh Pham <minh.pham@utexas.edu>
2017-04-14 00:47:50 -0500

    Implemented generic stepper function.

--- conway/src/conway/core.clj
+++ conway/src/conway/core.clj
@@ -17,24 +17,29 @@
 
 (def glider #{[2 0] [2 1] [2 2] [1 2] [0 1]})
 
+;; The state of the world can be entirely represented by the set of living
+;; cells. Cells that are not living are implied dead. To generate each
+;; successive state, count how many living neighbors (n) each cell has.
+(defn stepper
+  "Return a stepper function for Life-like cell automata.
+
+  Params:
+    neighbors(fn) takes a location & returns sequential collection of locations.
+    survive?(fn) and birth?(fn) are predicates on the number of living
+      neighbors.
+  "
+  [neighbors birth? survive?]
+  (fn [cells]
+    (set (for [[loc n] (frequencies (mapcat neighbors cells))
+               :when (if (cells loc) (survive? n) (birth? n))]
+           loc))))
+
 (defn neighbors
   "Get the indices of the cells next to a cell."
   [[x y]]
   (for [dx [-1 0 1] dy [-1 0 1] :when (not= 0 dx dy)]
     [(+ dx x) (+ dy y)]))
 
-;; The state of the world can be entirely represented by the set of living
-;; cells. Cells that are not living are implied dead. To generate each
-;; successive state, count how many living neighbors (n) each cell has.
-;; - n = 2 => same state
-;; - n = 3 => turn on
-;; - else  => turn off/die
-(defn step
-  "Yied the next state of the world."
-  [cells]
-  (set (for [[loc n] (frequencies (mapcat neighbors cells))
-             :when (or (= n 3) (and (= n 2) (cells loc)))]
-         loc)))
 
 (defn -main
   [& args]
@@ -42,7 +47,7 @@
 
   ;(pprint (populate (empty-board 6 6) (first (drop 8 (iterate step glider)))))
   (->>
-    (iterate step glider)
+    (iterate (stepper neighbors #{3} #{2 3}) glider)
     (drop 8)
     first
     (populate (empty-board 6 6))
